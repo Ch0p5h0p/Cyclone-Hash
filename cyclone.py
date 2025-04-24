304656ef724511f67165ceacb349071f214a1afe
Blooper7 <Samjpoyner@icloud.com>
2025-04-24 11:36:31 -0400

    Update cyclone.py

--- cyclone.py
+++ cyclone.py
@@ -17,9 +17,12 @@ def rotListLeft(data):
 def mix(state, chunk, tweak):
 	new_state=[0]*len(state)
 	
+	#print(f"Stats:\n  Current state: {state}\n  Selected chunk: {chunk}\n  State length: {len(state)}\n  Chunk length: {len(chunk)}")
 	for i in range(len(state)):
+		#print("--------------------")
 		#Add state and chunk (mod 256)
 		s=(state[i]+chunk[i])%256
+		#print(f"Added {state[i]} and {chunk[i]} (mod 256) to get {s}")
 		
 		#XOR the tweak with the accumulation to cause the tweak to diffuse widely
 		s=s^tweak[i]
@@ -27,9 +30,11 @@ def mix(state, chunk, tweak):
 		#Use neighbor value to introduce non-linearity into the hash and create dependency across the hash..
 		#Use (i+1)%len(state) to wrap around.
 		neighbor=state[(i+1)%len(state)]
+		#print(f"Collected neighbor value {neighbor}")
 		
 		#Rotate the neighbor left by (i&8)
 		rotation=((neighbor << (i % 8)) & 0xFF) | (neighbor >> (8 - (i % 8)))
+		#print(f"Generated left rotation from {neighbor} to {rotation}")
 		'''
 		The code above effectively demonstrates a bitwise left rotation, which is a shift with preservation of the high bits by wrapping them around instead of allowing them to go past the 8th bit.
 		1. (i % 8) : ensures the number of bits to rotate is between 0 and 7
@@ -46,17 +51,20 @@ def mix(state, chunk, tweak):
 		
 		'''
 		new_state[i]=s^rotation
+		#print(f"Setting new hash[{i}] to {s}^{rotation} ({s^rotation})")
 	return new_state
 
-def chunk(byteList, chunkByteSize=16):
+def chunk(byteList, msg_length, chunkByteSize=16):
 	# pad message
 	data=byteList
 	if len(data)%16!=0:
 		data.append(128)
 	while len(data)%16!=0:
 		data.append(0)
+	#print(f"Padded message to {len(data)} ({data})")
 	# chunk message
 	chunked=[data[(i*chunkByteSize):(i*chunkByteSize)+chunkByteSize] for i in range(len(data)//chunkByteSize)]
+	chunked+=[[msg_length]*chunkByteSize]
 	return chunked
 	
 def hashAlg(chunkList, tweak, rounds=40):
@@ -66,13 +74,15 @@ def hashAlg(chunkList, tweak, rounds=40):
 		hash.append(0)
 	# shift each chunk
 	for i in range(len(chunkList)):
-		orig=chunkList[i].copy()
 		for j in range(i):
 			rotListLeft(chunkList[i])
+		#print(f"Shifted {orig} to {chunkList[i]} (Shift rounds: {i})")
+	#print("----------")
 	
 	# Hash
 	for r in range(rounds):
 		for i in range(len(chunkList)):
+			#print(f"XORing {chunkList[i]} with hash ({hash})")
 			hash=mix(hash,chunkList[i],tweak)
 	return hash
 
@@ -83,6 +93,7 @@ def cleanupHash(hash:list):
 
 def hash(message, chunkLength=16, tweak=None):
 	message=msg2ord(message)
+	#print(f"Converted message into {message}")
 	if tweak!=None and len(tweak)!=chunkLength:
 		raise Exception(f"Tweak length must be {chunkLength} bytes long (Tweak {tweak} is only {len(tweak)})")
 	if tweak==None:
@@ -90,20 +101,32 @@ def hash(message, chunkLength=16, tweak=None):
 
 	tweakOrds=msg2ord(tweak)
 	
-	chunks=chunk(message, chunkLength)
+	chunks=chunk(message, len(message), chunkLength)
+	#print(f"Chunked message to {chunks}")
 	return (cleanupHash(hashAlg(chunks, tweakOrds)),tweak)
 
 def str2hex(string):
 	return bytes([ord(i) for i in string]).hex()
 
+help='''Usage: ./hash.py <message> <tweak>
+
+Takes in a string message parameter and an optional string tweak parameter and produces a hash output. If a tweak isn't provided, one is generated.
+	
+Examples:\n\t./hash.py \"Hello World!\"\n\t./hash.py \"Foo\" \"3GZO9NUL67pABvIZ\"
+'''
+
 if __name__ == "__main__":
+	msg=None
+	
 	try:
 		msg=sys.argv[1]
 	except:
-		print("Usage: ./hash.py <message> <tweak>\n")
-		print("Takes in a string message parameter and an optional string tweak parameter and produces a hash output. If a tweak isn't provided, one is generated.\n")
-		print("Examples:\n\t./hash.py \"Hello World!\"\n\t./hash.py \"Foo\" \"3GZO9NUL67pABvIZ\"")
-		exit()
+		try:
+			print("Using STDIN (^D ^D for EOF)")
+			msg=sys.stdin.read()
+			print("\n--------------------------------------")
+		except KeyboardInterrupt:
+			pass
 	try:
 		tweak=sys.argv[2]
 	except:
@@ -111,3 +134,16 @@ if __name__ == "__main__":
 	
 	out=hash(msg, tweak=tweak)
 	print(f"Hash: {str2hex(out[0])}\nTweak: {out[1]}")
+	
+'''
+How it works:
+	1. Did we get a tweak value? If not, generate one.
+	2. Pad the input data until it's a multiple of 16 bytes
+	3. Split the data into 16 byte chunks
+	4. Rotate the data left by the index it's at in the list of chunks
+	5. For every byte, add the current chunk byte at index i to the current state byte at index i modulo 256 and save as s
+	6. XOR the tweak byte at index i with the value from above. This causes it to diffuse widely.
+	7. Use the neighboring value in the state at index i to introduce interdependecy among bytes, and create a temporary left rotation from that byte by i mod 8
+	8. Set state[i] to the XOR of the rotation above and the s value
+	9. Repeat steps 5-8 40 times
+'''
